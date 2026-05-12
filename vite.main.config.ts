58d72fe05e94b11d58195755fa86a62ae4dcc45d
bundle-o-cells <austingpowel@gmail.com>
2026-05-12 12:08:24 -0400

    try hardcoded url

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   define: {
     __SERVER_URL__: JSON.stringify(
-      process.env.VITE_SERVER_URL ?? "https://beta.revolt.chat"
+      process.env.VITE_SERVER_URL ?? "https://hardcast.ing"
     ),
   },
 });
