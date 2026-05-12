61c9f36e6acf65ee0b5953ce1581f8198f47fe24
bundle-o-cells <austingpowel@gmail.com>
2026-05-12 11:40:01 -0400

    fix build_url variable

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -4,7 +4,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    // ...existing config...
     define: {
       __SERVER_URL__: JSON.stringify(env.VITE_SERVER_URL ?? "https://beta.revolt.chat"),
     },
